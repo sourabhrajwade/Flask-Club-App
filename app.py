02a2679bdea885555eec8257ff03e5647ef4d0b1
sourabhrajwade <rajwadesourabh@gmail.com>
2020-08-09 17:58:35 +0530

    requirement file fixed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 from forms import AddForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
